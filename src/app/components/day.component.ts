7656d681803f7bdb5e5a313bfdaf1ebd694b8c58
Eleazar <eleazar48a@gmail.com>
2020-12-29 16:44:58 -0600

    complete schedule day

--- src/app/components/day.component.ts
+++ src/app/components/day.component.ts
@@ -8,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 export class DayComponent implements OnInit {
   hours: number[] = [...Array(24).keys()];
 
-  listReminders: number[] = [2,5,9,13,17]
+  listReminders: any[] = [ 
+    {
+      text: 'this is my reminder',
+      day: 2,
+      color: '#6610f2',
+      hour: 4
+    },
+    {
+      text: 'an task important',
+      day: 2,
+      color: '#0dcaf0',
+      hour: 6
+    },
+    {
+      text: 'hello reminder :)',
+      day: 2,
+      color: '#20c997',
+      hour: 0
+    },
+    {
+      text: 'anything else',
+      day: 2,
+      color: '#dc3545',
+      hour: 10
+    },
+    // {
+    //   text: 'wherever...',
+    //   day: 2,
+    //   color: '#20c997',
+    //   hour: 4
+    // },
+    // {
+    //   text: 'other thing',
+    //   day: 2,
+    //   color: '#20c997',
+    //   hour: 4
+    // },
+  ];
 
   constructor() { }
+  clickOnReminder(reminder) {
+    console.log('reminder', reminder)
+  }
 
   ngOnInit(): void {
   }
