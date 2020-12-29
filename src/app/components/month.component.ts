1f20fa4efc8ca3c9507d4451ddaf4d62a9a9311d
Eleazar <eleazar48a@gmail.com>
2020-12-28 21:15:43 -0600

    design features of month

--- src/app/components/month.component.ts
+++ src/app/components/month.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-month',
   templateUrl: './month.component.html',
-  styleUrls: []
+  styleUrls: ['./month.component.css']
 })
 export class MonthComponent implements OnInit {
+  listDays: any = [29,30,...Array(31).keys(), 0,1];
+  month: string = 'Dicember'
 
   constructor() { }
+
+  onClickDay(day: number) {
+    console.log('day', day + 1)
+  }
 
   ngOnInit(): void {
   }
