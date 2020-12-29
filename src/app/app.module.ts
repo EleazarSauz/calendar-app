5888280a1a57422c715e4083a5de7c7e244f11aa
Eleazar <eleazar48a@gmail.com>
2020-12-28 18:55:47 -0600

    skeleton UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonthComponent } from './components/month.component';
+import { CalendarComponent } from './pages/calendar.component';
+import { DayComponent } from './components/day.component';
+import { FooterComponent } from './components/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonthComponent,
+    CalendarComponent,
+    DayComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day',
+  templateUrl: './day.component.html',
+  styles: []
+})
+export class DayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
